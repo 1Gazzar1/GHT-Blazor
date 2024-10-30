ae19273296669998722f41c909d84265eed168e1
mhmda <mhmdaljzar036@gmail.com>
2024-10-30 22:34:35 +0300

    added almost the rest of habit features

--- Gamified Habit Tracker Blazor.Client/Services/HabitService.cs
+++ Gamified Habit Tracker Blazor.Client/Services/HabitService.cs
@@ -28,5 +28,20 @@ public class HabitService
 		var response = await _httpClient.DeleteAsync($"api/Habit/{id}");
 		response.EnsureSuccessStatusCode();
 	}
+	public async Task CompleteHabit(int id)
+	{
+		var response = await _httpClient.PostAsync($"api/{id}/complete",null);
+		response.EnsureSuccessStatusCode();
+	}
+	public async Task<int> CurrentStreak(int user_id)
+    {
+        var streak = await _httpClient.GetFromJsonAsync<int>($"api/Streak/Current/{user_id}");
+        return streak;
+    }
+	public async Task<int> HighestStreak(int user_id)
+	{
+		var streak = await _httpClient.GetFromJsonAsync<int>($"api/Streak/Highest/{user_id}");
+		return streak;
+	}
 }
 
