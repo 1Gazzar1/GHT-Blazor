450879551b8454d60bce726771ff454f3c5ea270
mhmda <mhmdaljzar036@gmail.com>
2024-12-04 21:05:49 +0200

    added achievement completion functionality

--- Gamified Habit Tracker Blazor.Client/Services/AchievementService.cs
+++ Gamified Habit Tracker Blazor.Client/Services/AchievementService.cs
@@ -12,5 +12,16 @@ public class AchievementService
         var achievements = await _httpClient.GetFromJsonAsync<List<UserAchievementDTO>>($"api/Achievement/UserAchievements/{user_id}");
         return achievements;
     }
+	public async Task<List<bool>> CheckAllAchievements(int user_id)
+	{
+		var checks = await _httpClient.GetFromJsonAsync<List<bool>>($"api/Achievement/Check/{user_id}");
+        return checks ?? new(); 
+	}
+	public async Task CompleteAchievement(int achievement_id, int user_id)
+    {
+		var response = await _httpClient.PostAsJsonAsync($"api/Achievement/{achievement_id}/Complete",user_id);
+        response.EnsureSuccessStatusCode();
+	}
+	
 }
 
