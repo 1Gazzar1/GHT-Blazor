6d195aae9e2eb25eb2f457069be2929f0165530f
mhmda <mhmdaljzar036@gmail.com>
2024-12-05 14:56:40 +0200

    fixed a bug & added progress bar in profile page

--- Gamified Habit Tracker Blazor.Client/Services/LevelService.cs
+++ Gamified Habit Tracker Blazor.Client/Services/LevelService.cs
@@ -17,5 +17,14 @@ public class LevelService
 	    var response = await _httpClient.PostAsync($"api/Level/{user_id}/LevelUp",null);
         response.EnsureSuccessStatusCode(); 
     }
+	public async Task<Level> GetPastLvl(int user_id)
+	{
+		var token = ((CustomAuthProvider)_authState).GetToken();
+		_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+
+		var response = await _httpClient.GetFromJsonAsync<Level>($"api/Level/PastLevel/{user_id}");
+		return response;
+	}
 }
 
