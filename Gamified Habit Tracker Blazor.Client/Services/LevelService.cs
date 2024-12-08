da7454f7662caa92a73899f56c97522340599e55
mhmda <mhmdaljzar036@gmail.com>
2024-12-08 02:14:00 +0200

    added notification component & implemented it when completeing a habit or achievement

--- Gamified Habit Tracker Blazor.Client/Services/LevelService.cs
+++ Gamified Habit Tracker Blazor.Client/Services/LevelService.cs
@@ -8,14 +8,15 @@ public class LevelService
         _httpClient = httpClient;
         _authState = authState;
     }
-    public async Task CheckLevelUp(int user_id)
+    public async Task<bool> CheckLevelUp(int user_id)
     {
         var token = ((CustomAuthProvider)_authState).GetToken();
 		_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 	
 
 	    var response = await _httpClient.PostAsync($"api/Level/{user_id}/LevelUp",null);
-        response.EnsureSuccessStatusCode(); 
+        response.EnsureSuccessStatusCode();
+		return await response.Content.ReadFromJsonAsync<bool>();
     }
 	public async Task<Level> GetPastLvl(int user_id)
 	{
